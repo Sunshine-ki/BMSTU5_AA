4d7cb2f56b235859c66b01b839eddef62d3b81bb
Sukocheva Alice <sukocheva.alis@mail.ru>
2020-09-18 11:29:34 +0300

    Сдана

--- lab_01/main.py
+++ lab_01/main.py
@@ -22,6 +22,8 @@ def main():
     if distanceLev != distanceDamLev:
         output("Расстояние Дамерау-Левенштейна: " + str(distanceDamLev), GREEN)
 
+    LevenshteinRecursion(strFirst, strSecond)
+
 
 if __name__ == "__main__":
     main()
