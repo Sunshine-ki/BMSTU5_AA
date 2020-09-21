ab7f869a32c4139299302678e838efac79380ea4
Sukocheva Alice <sukocheva.alis@mail.ru>
2020-09-21 14:37:54 +0300

    Почти дописан отчет

--- lab_02/main.py
+++ lab_02/main.py
@@ -4,36 +4,34 @@ from color import *
 
 
 def main():
-    # output("МАТРИЦА A", BLUE)
-    # n = int(input(GREEN + "Введите кол-во строк: "))
-    # m = int(input(GREEN + "Введите кол-во столбцов: "))
-    # output("Введите матрицу:", GREEN)
-    # matrixA = Matrix(n, m, [[int(j) for j in input(GREEN).split()]
-    #                         for i in range(n)])
+    output("МАТРИЦА A", BLUE)
+    n = int(input(GREEN + "Введите кол-во строк: "))
+    m = int(input(GREEN + "Введите кол-во столбцов: "))
+    output("Введите матрицу:", GREEN)
+    matrixA = Matrix(n, m, [[int(j) for j in input(GREEN).split()]
+                            for i in range(n)])
 
-    # output("МАТРИЦА B", BLUE)
-    # k = int(input(GREEN + "Введите кол-во строк: "))
-    # l = int(input(GREEN + "Введите кол-во столбцов: "))
-    # output("Введите матрицу:", GREEN)
-    # matrixB = Matrix(k, l, [[int(j) for j in input(GREEN).split()]
-    #                         for i in range(k)])
+    output("МАТРИЦА B", BLUE)
+    k = int(input(GREEN + "Введите кол-во строк: "))
+    l = int(input(GREEN + "Введите кол-во столбцов: "))
+    output("Введите матрицу:", GREEN)
+    matrixB = Matrix(k, l, [[int(j) for j in input(GREEN).split()]
+                            for i in range(k)])
 
-    # if m != k:
-    #     output("Некорректные размеры матриц!", RED)
-    #     return
+    if m != k:
+        output("Некорректные размеры матриц!", RED)
+        return
 
-    # matrixC = WinogradOptimization(matrixA, matrixB)
-    # matrixC.output()
-
-    matrixA = Matrix(3, 2, [[2, 3], [1, 0], [2, 2]])
-    matrixB = Matrix(2, 4, [[2, 2, 1, 9], [4, 2, 8, 1]])
-    matrixC = WinogradMult(matrixA, matrixB)
+    matrixC = multiplication(matrixA, matrixB)
+    print('multiplication')
     matrixC.output()
-
-    # matrixA = Matrix(3, 3, [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-    # matrixB = Matrix(3, 3, [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-    # matrixC = WinogradOptimization(matrixA, matrixB)
+    # matrixC = WinogradMult(matrixA, matrixB)
+    # print('Winograd multiplication')
     # matrixC.output()
+    # matrixC = WinogradOptimization(matrixA, matrixB)
+    # print('WinogradOptimization')
+    # matrixC.output()
+
 
 
 if __name__ == "__main__":
