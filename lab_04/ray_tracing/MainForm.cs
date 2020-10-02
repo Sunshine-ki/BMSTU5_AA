74506bd4e7daa3c8ca8e13a1a2f24a994333a25f
Sunshine-ki <sukocheva.alis@mail.ru>
2020-10-02 22:09:30 +0300

    Параллелим трассировку лучей

---
+++ lab_04/ray_tracing/MainForm.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Timers;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+using System.Threading;
+
+namespace Newton
+{
+	public class Limit
+	{
+		public Int32 begin;
+		public Int32 end;
+		public Limit(Int32 begin, Int32 end)
+		{
+			this.begin = begin;
+			this.end = end;
+		}
+
+	}
+	public class MainForm : Form
+	{
+		private List<Shape> _scene;
+		private Bitmap _img; // Содержит растровое изображение.
+		private PictureBox _imgBox; // Будет сожержать само изображение.
+		public MainForm(List<Shape> scene)
+		{
+			SettingsWindows();
+			InitializeComponent();
+			_scene = scene;
+		}
+
+		#region SETTINGS
+		private void SettingsWindows()
+		{
+			this.Text = "Newton\'s cradle";
+			this.Width = (int)SizeObjects.WidthWindows;
+			this.Height = (int)SizeObjects.HeightWindows;
+			// Стиль рамки: 
+			// Sizable - изменяем размер (по умолчанию).
+			// None - без рамки.
+			// Fixed3D - Фиксированная трехмерная граница.
+			this.FormBorderStyle = FormBorderStyle.Sizable;
+			// Убираем возможность развернуть окно во весь экран и сложить.
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			// Устанавливаем начальное положение окна в центре.
+			this.StartPosition = FormStartPosition.CenterScreen;
+		}
+		private void InitializeComponent()
+		{
+			// Button Off/On.
+			Button buttonOffOn = new Button();
+			buttonOffOn.Text = "off/on";
+			buttonOffOn.Location = new Point(550, 800);
+			this.AcceptButton = buttonOffOn;
+			buttonOffOn.Click += new EventHandler(ButtonOffOnClick);
+			this.Controls.Add(buttonOffOn);
+
+			// Image.
+			_imgBox = new PictureBox();
+			_imgBox.Size = new Size((int)SizeObjects.WidthCanvas, (int)SizeObjects.HeightCanvas);
+			this.Controls.Add(_imgBox);
+			_img = new Bitmap((int)SizeObjects.WidthCanvas, (int)SizeObjects.HeightCanvas);
+			// _imgBox.Image = _img;
+
+		}
+		#endregion
+
+		private void ButtonOffOnClick(object sender, System.EventArgs e)
+		{
+			DrawScene();
+		}
+
+
+		public void func(object obj)
+		{
+			Limit limit = (Limit)obj;
+			for (Int32 i = limit.begin; i < limit.end; i++)
+				for (Int32 j = 0; j < (int)SizeObjects.HeightCanvas; j++)
+					_img.SetPixel(i, j, TraceRay(new Point(i, j)));
+
+		}
+
+		private void DrawScene()
+		{
+			List<Thread> listThread = new List<Thread>();
+
+			int step = 50;
+			for (int i = 0; i < (int)SizeObjects.WidthCanvas; i += step)
+			{
+				listThread.Add(new Thread(new ParameterizedThreadStart(func)));
+				listThread[listThread.Count - 1].Start(new Limit(i, i + step));
+			}
+
+			// Join — Это метод синхронизации, который блокирует вызывающий поток (то есть поток, который вызывает метод).
+			// Используйте этот метод, чтобы убедиться, что поток был завершен.
+			// То есть мы не пойдем далее по коду, пока что не выполнятся потоки, вызванные ранее 
+			// (то есть те потоки, которые мы джоиним.).
+			foreach (var elem in listThread)
+			{
+				elem.Join();
+			}
+
+			_imgBox.Image = _img;
+
+			// for (Int32 i = 0; i < (int)SizeObjects.WidthCanvas; i++)
+			// 	for (Int32 j = 0; j < (int)SizeObjects.HeightCanvas; j++)
+			// 		_img.SetPixel(i, j, TraceRay(new Point(i, j)));
+			// _imgBox.Image = _img;
+
+		}
+
+		private Color TraceRay(Point point)
+		{
+			Shape ClosestObject = null;
+			double MinZ = Double.NegativeInfinity;
+
+			foreach (var elem in _scene)
+			{
+				if (IsVisible((elem as Sphere).Radius, elem.Center.X - point.X, elem.Center.Y - point.Y))
+				{
+					if (elem.Center.Z > MinZ)
+					{
+						MinZ = elem.Center.Z;
+						ClosestObject = elem;
+					}
+				}
+			}
+
+			if (ClosestObject == null)
+				return Color.Black;
+
+			Colors result = ClosestObject.Clr;
+			return Color.FromArgb(result.R, result.G, result.B);
+		}
+		private bool IsVisible(double R, double dx, double dy)
+		{
+			if (Math.Pow(dx, 2.0d) + Math.Pow(dy, 2.0d) <= R * R)
+				return true;
+			return false;
+		}
+
+	}
+}
---
+++ lab_04/src/Program.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Threading;
+class Program
+{
+	static Mutex mutexObj = new Mutex();
+	static int x = 0;
+
+	static void Main(string[] args)
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			Thread myThread = new Thread(Count);
+			myThread.Name = $"Поток {i}";
+			myThread.Start();
+		}
+
+		Console.ReadLine();
+	}
+	public static void Count()
+	{
+		mutexObj.WaitOne();
+		x = 1;
+		for (int i = 1; i < 9; i++)
+		{
+			Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+			x++;
+			Thread.Sleep(100);
+		}
+		mutexObj.ReleaseMutex();
+	}
+}
+// class Program
+// {
+// 	static void Main(string[] args)
+// 	{
+// 		int number = 4;
+// 		// создаем новый поток
+// 		Thread myThread = new Thread(new ParameterizedThreadStart(Count));
+// 		myThread.Start(number);
+
+// 		for (int i = 1; i < 9; i++)
+// 		{
+// 			Console.WriteLine("Главный поток:");
+// 			Console.WriteLine(i * i);
+// 			Thread.Sleep(300);
+// 		}
+
+// 		Console.ReadLine();
+// 	}
+
+// 	public static void Count(object x)
+// 	{
+// 		for (int i = 1; i < 9; i++)
+// 		{
+// 			int n = (int)x;
+
+// 			Console.WriteLine("Второй поток:");
+// 			Console.WriteLine(i * n);
+// 			Thread.Sleep(400);
+// 		}
+// 	}
+// }
+
+
+
+// class Program
+// {
+// 	static void Main(string[] args)
+// 	{
+// 		// создаем новый поток
+// 		Thread myThread = new Thread(new ThreadStart(Count));
+// 		myThread.Start(); // запускаем поток
+
+// 		for (int i = 1; i < 19; i++)
+// 		{
+// 			Console.WriteLine("Главный поток:");
+// 			Console.WriteLine(i * i);
+// 			Thread.Sleep(300);
+// 		}
+
+// 	}
+
+// 	public static void Count(int a, int b)
+// 	{
+// 		for (int i = 1; i < 19; i++)
+// 		{
+// 			Console.WriteLine("Второй поток:");
+// 			Console.WriteLine(i * i);
+// 			Thread.Sleep(400);
+// 		}
+// 	}
+// }
+
+
+// // using System;
+// // using System.Threading;
+
+// // namespace Lab_04
+// // {
+// // 	static class Program
+// // 	{
+// // 		public static void CallToChildThread()
+// // 		{
+// // 			// Console.WriteLine("Child thread starts");
+// // 			for (int i = 0; i < 10; i++)
+// // 				Console.WriteLine("Поток 1");
+// // 		}
+// // 		static void Main(string[] args)
+// // 		{
+// // 			Thread childThread = new Thread(new ThreadStart(CallToChildThread));
+// // 			childThread.Start();
+// // 			for (int i = 0; i < 10; i++)
+// // 				Console.WriteLine("Главный поток");
+// // 			// childThread2.Start();
+// // 		}
+// // 	}
+// // }
