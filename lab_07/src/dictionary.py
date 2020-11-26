e2d4f42d9cbdf1015e3a9c2e150337c3426cf48b
Sunshine-ki <sukocheva.alis@mail.ru>
2020-11-26 11:08:01 +0300

    Дописан отчет

--- lab_07/src/dictionary.py
+++ lab_07/src/dictionary.py
@@ -1,118 +1,115 @@
 from world_info import WorldInfo
 
+
 class Dictionary(object):
-	data = dict()
+    data = dict()
 
-	def __init__(self, file_name):
-		self.LoadData(file_name)
+    def __init__(self, file_name):
+        self.LoadData(file_name)
 
-	def LoadData(self, file_name):
-		f = open(file_name, 'r')
+    def LoadData(self, file_name):
+        f = open(file_name, 'r')
 
-		for line in f:
-			array = line.split(',') 
-			key = array[1]
-			self.data[key[1:-1]] = WorldInfo(array[2], 
-			int(array[3]), array[4],int(array[5]),int(array[6]))
-			# print(self.data[array[1]])
-			
-		f.close()
+        for line in f:
+            array = line.split(',')
+            key = array[1]
+            self.data[key[1:-1]] = WorldInfo(array[2],
+                                             int(array[3]), array[4], int(array[5]), int(array[6]))
+            # print(self.data[array[1]])
 
-	def Log(self):
-		for elem in self.data:
-			print(elem)
-			print(self.data.setdefault(elem))
+        f.close()
 
-	def __getitem__(self, key):
-		return self.data.get(key)
+    def Log(self):
+        for elem in self.data:
+            print(elem)
+            print(self.data.setdefault(elem))
 
-	def sorting_by_keys(self):
-		new_dict = dict()
-		list_keys = list(self.data.keys())
-		list_keys.sort()
+    def __getitem__(self, key):
+        return self.data.get(key)
 
-		for i in list_keys:
-			new_dict[i] = self.data[i]
-			# print(i, ':', self.data[i])
+    def sorting_by_keys(self):
+        new_dict = dict()
+        list_keys = list(self.data.keys())
+        list_keys.sort()
 
-		return new_dict
+        for i in list_keys:
+            new_dict[i] = self.data[i]
+            # print(i, ':', self.data[i])
 
-	def sorting_by_values(self, data):
-		new_dict = dict()
+        return new_dict
 
-		list_d = list(data.items())
-		list_d.sort(key=lambda i: i[1], reverse=True)
-		for elem in list_d:
-			# print(elem[0], elem[1])
-			new_dict[elem[0]] = elem[1]
+    def sorting_by_values(self, data):
+        new_dict = dict()
 
-		return new_dict
+        list_d = list(data.items())
+        list_d.sort(key=lambda i: i[1], reverse=True)
+        for elem in list_d:
+            # print(elem[0], elem[1])
+            new_dict[elem[0]] = elem[1]
 
-	def SequentialSearch(self, key):
-		for elem in self.data:
-			if key == elem:
-				return self.data[elem]
-		return -1
+        return new_dict
 
+    def SequentialSearch(self, key):
+        for elem in self.data:
+            if key == elem:
+                return self.data[elem]
+        return -1
 
-	def BinarySearch(self, key, list_keys):
-		l, r = 0, len(list_keys) - 1
+    def BinarySearch(self, key, list_keys):
+        l, r = 0, len(list_keys) - 1
 
-		while l <= r:
-			middle = (r + l) // 2 # TODO: Оптимизация ? (l + (r - l) // 2)
-			elem = list_keys[middle]
+        while l <= r:
+            middle = (r + l) // 2  # Оптимизация ? (l + (r - l) // 2)
+            elem = list_keys[middle]
 
-			if elem == key:
-				return self.data[elem]
+            if elem == key:
+                return self.data[elem]
 
-			elif elem < key:
-				l = middle + 1
-			
-			else:
-				r = middle - 1
+            elif elem < key:
+                l = middle + 1
 
-		return -1
+            else:
+                r = middle - 1
 
-	def parser(self):
-		# Разбиваем по буквам алфавита и
-		# Сортируем (по убыванию) по кол-ву элементов в каждой букве.
-		count_dict = {i: 0 for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
-		# count_dict будет содрежать
-		# Кол-во слов для каждой буквы.
-		for key in self.data:
-			count_dict[key[0]] += 1
+        return -1
 
-		# Сортируем по убыванию значений.
-		count_dict = self.sorting_by_values(count_dict)
+    def parser(self):
+        # Разбиваем по буквам алфавита и
+        # Сортируем (по убыванию) по кол-ву элементов в каждой букве.
+        count_dict = {i: 0 for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
+        # count_dict будет содрежать
+        # Кол-во слов для каждой буквы.
+        for key in self.data:
+            count_dict[key[0]] += 1
 
-		# Создаем новый словарь (он будет отсортирован
-		# по убыванию кол-ва значений.
-		new_dict = {i: dict() for i in count_dict}
+        # Сортируем по убыванию значений.
+        count_dict = self.sorting_by_values(count_dict)
 
-		# Заполняем новый словарь.
-		for key in self.data:
-			new_dict[key[0]].update({key: self.data[key]})
+        # Создаем новый словарь (он будет отсортирован
+        # по убыванию кол-ва значений.
+        new_dict = {i: dict() for i in count_dict}
 
-		# for key in new_dict:
-		# 	print(key, '\n\n')
-		# 	for elem in new_dict[key]:
-		# 		print(elem, new_dict[key][elem], "\n")
+        # Заполняем новый словарь.
+        for key in self.data:
+            new_dict[key[0]].update({key: self.data[key]})
 
-		return new_dict
+        # for key in new_dict:
+        # 	print(key, '\n\n')
+        # 	for elem in new_dict[key]:
+        # 		print(elem, new_dict[key][elem], "\n")
 
-	def Search(self, key, new_dict):
-		for k in new_dict:
-			# Если нашли нужную букву
-			if key[0] == k:
-				# То начинаем искать.
-				for elem in new_dict[k]:
-					if elem == key:
-						return new_dict[k][elem]
-				# Если не нашли среди слов, которые были
-				# На искомую букву, значит в словаре нет такого слова.
-				return -1
-		
-		return -1
+        return new_dict
 
+    def Search(self, key, new_dict):
+        for k in new_dict:
+            # Если нашли нужную букву
+            if key[0] == k:
+                # То начинаем искать.
+                for elem in new_dict[k]:
+                    if elem == key:
+                        return new_dict[k][elem]
+                # Если не нашли среди слов, которые были
+                # На искомую букву, значит в словаре нет такого слова.
+                return -1
 
-
+        return -1
--- lab_07/src/main.py
+++ lab_07/src/main.py
@@ -6,50 +6,52 @@ GREEN = '\033[32m'
 YELLOW = '\033[33m'
 BLUE = '\033[34m'
 
+
 def main():
-	data = Dictionary('world.csv')
-	
-	print(YELLOW)
-	key = input("Input key: ")
-	if key == '':
-		return
+    data = Dictionary('world.csv')
 
-	print(BLUE)
-	print("\nValue:\n{0}\n".format(data.SequentialSearch(key)))
+    print(YELLOW)
+    key = input("Input key: ")
+    if key == '':
+        return
 
-	t1 = clock()
-	for _ in range(COUNT):
-		data.SequentialSearch(key)
-	t2 = clock()
-	print(GREEN)
-	print("Time sequential search: {0}\n".format(t2 - t1))
+    print(BLUE)
+    print("\nValue:\n{0}\n".format(data.SequentialSearch(key)))
 
-	t1 = clock()
-	new_dict = data.sorting_by_keys() 
-	list_keys = list(new_dict.keys()) 
-	t2 = clock()
-	print(YELLOW)
-	print("Sorting time : {0}".format(t2 - t1))
+    t1 = clock()
+    for _ in range(COUNT):
+        data.SequentialSearch(key)
+    t2 = clock()
+    print(GREEN)
+    print("Time sequential search: {0}\n".format(t2 - t1))
 
-	t1 = clock()
-	for _ in range(COUNT):
-		data.BinarySearch(key, list_keys)
-	t2 = clock()
-	print(GREEN)
-	print("Time binary search: {0}\n".format(t2 - t1))
+    t1 = clock()
+    new_dict = data.sorting_by_keys()
+    list_keys = list(new_dict.keys())
+    t2 = clock()
+    # print(YELLOW)
+    # print("Sorting time : {0}".format(t2 - t1))
 
-	t1 = clock()
-	new_dict = data.parser()
-	t2 = clock()
-	print(YELLOW)
-	print("Data preparation: {0}".format(t2 - t1))
+    t1 = clock()
+    for _ in range(COUNT):
+        data.BinarySearch(key, list_keys)
+    t2 = clock()
+    print(GREEN)
+    print("Time binary search: {0}\n".format(t2 - t1))
 
-	t1 = clock()
-	for _ in range(COUNT):
-		data.Search(key, new_dict)
-	t2 = clock()
-	print(GREEN)
-	print("Time search: {0}".format(t2 - t1))
+    t1 = clock()
+    new_dict = data.parser()
+    t2 = clock()
+    # print(YELLOW)
+    # print("Data preparation: {0}".format(t2 - t1))
+
+    t1 = clock()
+    for _ in range(COUNT):
+        data.Search(key, new_dict)
+    t2 = clock()
+    print(GREEN)
+    print("Time search: {0}".format(t2 - t1))
+
 
 if __name__ == "__main__":
-	main();
+    main()
